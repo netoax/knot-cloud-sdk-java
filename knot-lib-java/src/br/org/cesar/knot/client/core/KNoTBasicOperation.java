8f4beb8616c02af9a075816f7b4343c453ad2646
João Neto <j.alexandreeneto@gmail.com>
2019-02-02 22:03:55 -0300

    Bootstrap project

---
+++ knot-lib-java/src/br/org/cesar/knot/client/core/KNoTBasicOperation.java
@@ -0,0 +1,19 @@
+package br.org.cesar.knot.client.core;
+
+public interface KNoTWebSocketOperation {
+
+	public void connect();
+	
+	public void registerDevice();
+	
+	public void getDevices();
+	
+	public void updateMetadata();
+	
+	public void unregisterDevice();
+	
+	public void activateDevice();
+	
+	public void createSessionToken();
+	
+}
---
+++ knot-lib-java/src/br/org/cesar/knot/client/core/WebSocketConnection.java
@@ -0,0 +1,41 @@
+package br.org.cesar.knot.client.core;
+
+public class WebSocketConnectionImpl implements WebSocketConnection {
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
